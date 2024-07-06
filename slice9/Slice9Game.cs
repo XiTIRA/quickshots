8cfbf5017ccc743e329f67825b5647a776b1d7a8
theLaffa <robert@laffey.au>
2024-07-06 22:01:27 +1000

    Impliment simple slice9 class

--- slice9/Slice9Game.cs
+++ slice9/Slice9Game.cs
@@ -9,6 +9,9 @@ public class Slice9Game : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    private Slice9 _window1;
+    private Slice9 _window2;
+
     public Slice9Game()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -27,7 +30,17 @@ public class Slice9Game : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
+        _window1 = new Slice9(
+            Content.Load<Texture2D>("panel"),
+            30,
+            new Vector2(50, 50),
+            new Vector2(200, 350));
+
+        _window2 = new Slice9(
+            Content.Load<Texture2D>("panel"),
+            30,
+            new Vector2(300, 50),
+            new Vector2(400, 250));
     }
 
     protected override void Update(GameTime gameTime)
@@ -45,23 +58,114 @@ public class Slice9Game : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _window1.Draw(_spriteBatch);
+        _window2.Draw(_spriteBatch);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
     
     public class Slice9
     {
-       private Rectangle TopLeft;
-       private Rectangle TopRight;
-       private Rectangle BottomLeft;
-       private Rectangle BottomRight;
-       private Rectangle Top;
+       private Rectangle _topLeft;
+       private Rectangle _topRight;
+       private Rectangle _bottomLeft;
+       private Rectangle _bottomRight;
+       private Rectangle _top;
        private Rectangle _bottom;
        private Rectangle _left;
        private Rectangle _right;
        private Rectangle _center;
 
        private Texture2D _texture;
+       private Vector2 _position;
+       private Vector2 _size;
+       private int _fromEdge;
+
+
+       public Slice9(Texture2D texture, int fromEdge, Vector2 position, Vector2 size)
+       {
+           _texture = texture;
+           _fromEdge = fromEdge;
+
+           int width = _texture.Width;
+           int height = _texture.Height;
+
+           _position = position;
+           _size = size;
+
+           _topLeft = new Rectangle(0,0,fromEdge,fromEdge);
+           _top = new Rectangle(fromEdge,0,width-fromEdge*2,fromEdge);
+           _topRight = new Rectangle(width-fromEdge,0,fromEdge,fromEdge);
+           _left = new Rectangle(0,fromEdge,fromEdge,height-fromEdge*2);
+           _center = new Rectangle(fromEdge,fromEdge,width-fromEdge*2,height-fromEdge*2);
+           _right = new Rectangle(width-fromEdge,fromEdge,fromEdge,height-fromEdge*2);
+           _bottomLeft = new Rectangle(0,height-fromEdge,fromEdge,fromEdge);
+           _bottom = new Rectangle(fromEdge,height-fromEdge,width-fromEdge*2,fromEdge);
+           _bottomRight = new Rectangle(width-fromEdge,height-fromEdge,fromEdge,fromEdge);
+       }
+
+       public void Draw(SpriteBatch spriteBatch)
+       {
+            spriteBatch.Draw(
+               _texture,
+               new Rectangle((int)_position.X,(int)_position.Y,_fromEdge,_fromEdge),
+               _topLeft,
+               Color.White
+           );
+           spriteBatch.Draw(
+               _texture,
+               new Rectangle((int)(_position.X+_size.X-_fromEdge),(int)_position.Y,_fromEdge,_fromEdge),
+               _topRight,
+               Color.White
+           );
+
+           spriteBatch.Draw(
+               _texture,
+               new Rectangle((int)(_position.X),(int)(_position.Y+_size.Y-_fromEdge),_fromEdge,_fromEdge),
+               _bottomLeft,
+               Color.White
+           );
+           spriteBatch.Draw(
+               _texture,
+               new Rectangle((int)(_position.X+_size.X-_fromEdge),(int)(_position.Y+_size.Y-_fromEdge),_fromEdge,_fromEdge),
+               _bottomRight,
+               Color.White
+           );
+
+           spriteBatch.Draw(
+               _texture,
+               new Rectangle((int)(_position.X),(int)(_position.Y+_fromEdge),_fromEdge,(int)(_size.Y-(_fromEdge*2))),
+               _left,
+               Color.White
+           );
+           spriteBatch.Draw(
+               _texture,
+               new Rectangle((int)(_position.X+_size.X-_fromEdge),(int)(_position.Y+_fromEdge),_fromEdge,(int)(_size.Y-(_fromEdge*2))),
+               _right,
+               Color.White
+           );
+
+           spriteBatch.Draw(
+               _texture,
+               new Rectangle((int)(_position.X+_fromEdge),(int)(_position.Y),(int)(_size.X-(_fromEdge*2)),_fromEdge),
+               _top,
+               Color.White
+           );
+           spriteBatch.Draw(
+               _texture,
+               new Rectangle((int)(_position.X+_fromEdge),(int)(_position.Y+_size.Y-_fromEdge),(int)(_size.X-(_fromEdge*2)),_fromEdge),
+               _bottom,
+               Color.White
+           );
+
+           spriteBatch.Draw(
+               _texture,
+               new Rectangle((int)(_position.X+_fromEdge),(int)(_position.Y+_fromEdge),(int)(_size.X-(_fromEdge*2)),(int)(_size.Y-(_fromEdge*2))),
+               _center,
+               Color.White
+           );
+       }
     }
 }
